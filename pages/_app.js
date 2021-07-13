db9c6c87a636cf19e8fb87d1ca281da683a517d7
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-07-13 19:35:35 +0200

    create UserContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import { Toaster } from 'react-hot-toast'
+import { UserContext } from '../lib/context'
 import Navbar from '../components/NavBar'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <UserContext.Provider value={{ user: {}, username: 'florianmg' }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   )
 }
 
