0166f4d7039627a35e94eaed583f1ee2bbba7fb6
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-07-13 19:53:10 +0200

    Create Hook to read auth record and user profile doc

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import { Toaster } from 'react-hot-toast'
+
+import { useUserData } from '../lib/hooks'
 import { UserContext } from '../lib/context'
+
 import Navbar from '../components/NavBar'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData()
   return (
-    <UserContext.Provider value={{ user: {}, username: 'florianmg' }}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
