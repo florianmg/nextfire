cdae962e9d22452a5ad4a40b1ba6122b195eccd0
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-07-13 18:25:36 +0200

    install react-hot-toaster

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { Toaster } from 'react-hot-toast'
 import Navbar from '../components/NavBar'
 
 import '../styles/globals.css'
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </>
   )
 }
