add86f8863221cfad6203a7ec706e1770d2be9fa
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-07-13 18:15:43 +0200

    create Loader component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Loader from '../components/Loader'
 
 export default function Home() {
-  return <div></div>
+  return (
+    <main>
+      <Loader show />
+    </main>
+  )
 }
